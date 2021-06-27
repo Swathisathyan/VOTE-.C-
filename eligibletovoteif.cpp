4a9ca57e8d47501da51f214901fa22846998a28b
Swathisathyan <82638675+Swathisathyan@users.noreply.github.com>
2021-06-27 22:47:56 +0530

    Create eligibletovoteif.cpp

---
+++ eligibletovoteif.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+main()
+{int age;
+cout<<"\n**""Hey @ @!!Check out your Eligibility to cast your vote for this election **\n       ~~\n";
+cout<<"Enter your age here :";
+cin>>age;
+if(age>=18)
+{cout<<"You are eligible to vote!"; }
+else cout<<"Sorry,you are not eligible to vote";
+
+
+
+
+
+}
